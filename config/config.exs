76574d570721ee1f1dd276d944c74c7ea7733915
Luke Ledet <luke@revelry.co>
2018-01-19 15:46:55 -0600

    Send output to tty

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,9 @@ config :bootloader,
   init: [:nerves_runtime],
   app: Mix.Project.config[:app]
 
+config :nerves, :firmware,
+  rootfs_overlay: "rootfs_overlay"
+
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
